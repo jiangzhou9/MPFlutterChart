ed24f610b5583cda2fd1e1c7a843a6d6e20fccd9
jiangzhou <976912502@qq.com>
2021-04-26 14:28:02 +0800

    futureor bug

--- mp_chart/lib/mp/chart/chart.dart
+++ mp_chart/lib/mp/chart/chart.dart
@@ -46,11 +46,9 @@ abstract class ChartState<T extends Chart> extends State<T> {
 
     String fileName = DateTime.now().toIso8601String();
     String path = '$directory/$fileName.png';
-    _screenshotController.capture(path: path, pixelRatio: 3.0).then((imgFile) {
-      ImageGallerySaver.saveImage(Uint8List.fromList(imgFile.readAsBytesSync()))
-          .then((value) {
-        imgFile.delete();
-      });
+    _screenshotController.capture(path: path, pixelRatio: 3.0).then((imgFile) async {
+      await ImageGallerySaver.saveImage(Uint8List.fromList(imgFile.readAsBytesSync()));
+      imgFile.delete();
       isCapturing = false;
     }).catchError((error) {
       isCapturing = false;
